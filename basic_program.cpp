72b1ab5b3d64b2cbea91a5d4be90b79f9e185ffc
evanpeltgmu <evanpelt@gmu.edu>
2025-06-20 10:51:50 -0400

    Update basic_program.cpp

--- basic_program.cpp
+++ basic_program.cpp
@@ -3,5 +3,5 @@ using namespace std;
 
 int main(){
   cout<<"I am updating the basic program in a different branch"<<'\n';
-  cout<<"I will merge this with the main branch<<'\n';
+  cout<<"I will merge this with the main branch"<<'\n';
 }
